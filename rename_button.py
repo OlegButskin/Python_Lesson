9aca9a95be41607fbbc2ea80d4ed13a67315d690
OlegButskin <olegskypro.it@mail.ru>
2025-05-26 18:23:13 +0300

    Add files via upload

---
+++ rename_button.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/textinput")
+
+driver.find_element(By.CSS_SELECTOR,"#newButtonName").send_keys("Skypro")
+driver.implicitly_wait(10)
+driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
+print(driver.find_element(By.CSS_SELECTOR,"#updatingButton").text)
