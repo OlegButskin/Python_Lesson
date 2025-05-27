32f7c568fc906dc34d510c8c17568d70a406e126
OlegButskin <olegskypro.it@mail.ru>
2025-05-27 14:33:00 +0300

    press_button.py

--- press_button.py
+++ press_button.py
@@ -3,12 +3,9 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(
-service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get("http://uitestingplayground.com/ajax")
-
-driver.implicitly_wait(20)
 
 driver.find_element(By.ID, "ajaxButton").click()
 
