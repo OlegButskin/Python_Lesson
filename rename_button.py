252fe3ccd0f7ce90379a6ab97d367a23bfdf16bc
OlegButskin <olegskypro.it@mail.ru>
2025-05-27 14:33:26 +0300

    ename_button.py

--- rename_button.py
+++ rename_button.py
@@ -3,8 +3,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(
-service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 driver.get("http://uitestingplayground.com/textinput")
 
