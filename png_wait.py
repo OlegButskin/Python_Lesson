a5958c0914b6e3baf48c988ced278d058644d36c
OlegButskin <olegskypro.it@mail.ru>
2025-05-27 14:31:49 +0300

    png_wait.py

--- png_wait.py
+++ png_wait.py
@@ -13,6 +13,6 @@ driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
 waiter = WebDriverWait(driver, 30)
 
-waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
+waiter.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#landscape")))
 
 print(driver.find_element(By.CSS_SELECTOR, "#award").get_dom_attribute("src"))
