b98341a0788776a3f2f12e19b7a072ca74314eb8
OlegButskin <olegskypro.it@mail.ru>
2025-05-29 21:45:02 +0300

    test_02_calc.py

--- test_02_calc.py
+++ test_02_calc.py
@@ -1,34 +1,30 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-driver.maximize_window()
 
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+def test_calculator_result():
+    driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
+    wait = WebDriverWait(driver, 10)
+    delay_input = wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#delay"))
+    )
+    delay_input.clear()
+    delay_input.send_keys("45")
 
-wait = WebDriverWait(driver, 10)
-delay = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#delay")))
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
 
-delay.clear()
-delay.send_keys("45")
+    WebDriverWait(driver, 50).until(
+        EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
+    )
+    result = driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert result == "15"
 
-
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.XPATH, "//span[text()='=']").click()
-
-
-WebDriverWait(driver, 50).until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), '15')
-)
-
-
-res = driver.find_element(By.CSS_SELECTOR, ".screen").text
-assert res == '15', f"Ожидалось '15', но получено '{res}'"
-
-driver.quit()
+    driver.quit()
