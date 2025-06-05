ce5beed6cf1bdb36d0fa87e7506ad56ca6d909c1
OlegButskin <olegskypro.it@mail.ru>
2025-06-05 14:55:01 +0300

    press_button.py

--- press_button.py
+++ press_button.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
@@ -12,5 +10,3 @@ driver.find_element(By.ID, "ajaxButton").click()
 element = driver.find_element(By.CSS_SELECTOR, "p.bg-success")
 
 print(element.text)
-
-
